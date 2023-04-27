61d1578340acb7e513d8b122f6341f1ee93b9252
Tudor <tudorcristianmurariu@yahoo.com>
2023-04-27 22:34:58 +0300

    Sing in and log in pages have a html, also added a main app component that is empty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './log-in/login/login.component';
+import { SignUpComponent } from './sign-up/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'main-app', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './log-in/login/login.component';
+import { SignUpComponent } from './sign-up/sign-up/sign-up.component';
 import { AppComponent } from './app.component';
+import { MainAppComponent } from './main-app/main-app/main-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    SignUpComponent,
+    AppComponent,
+    MainAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
